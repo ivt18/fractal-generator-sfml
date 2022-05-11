40da378a84fddc80f1dc27293bca5e52358335d0
IliyanVT18 <iliyanvt18@gmail.com>
2022-05-11 20:37:51 +0200

    Initial commit.

---
+++ src/main.cpp
@@ -0,0 +1,24 @@
+#include <SFML/Graphics.hpp>
+
+#define SCREEN_X 800
+#define SCREEN_Y 600
+
+int main() {
+    sf::RenderWindow window(sf::VideoMode(SCREEN_X, SCREEN_Y), "hello");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+    
+    while(window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
+    return 0;
+}
