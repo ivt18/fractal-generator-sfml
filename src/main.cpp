501441237b251487e37cd26f8257d521ae1c0449
IliyanVT18 <iliyanvt18@gmail.com>
2022-05-11 20:54:38 +0200

    Initial commit.

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,11 @@
+#include <iostream>
 #include <SFML/Graphics.hpp>
 
 #define SCREEN_X 800
 #define SCREEN_Y 600
 
 int main() {
-    sf::RenderWindow window(sf::VideoMode(SCREEN_X, SCREEN_Y), "hello");
+    sf::RenderWindow window(sf::VideoMode(SCREEN_X, SCREEN_Y), "Fractal Generator");
     sf::CircleShape shape(100.f);
     shape.setFillColor(sf::Color::Green);
     
